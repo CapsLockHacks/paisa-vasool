cf677e2ce9bf0ce150284290ba0aa9e716a74e26
Rohan Verma <hello@rohanverma.net>
2019-10-20 06:16:40 +0530

    feat: gen upi link long url

--- django/upi/utils.py
+++ django/upi/utils.py
@@ -1,3 +1,6 @@
+import requests
+import json
+import base64
 from upi.models import Group, Contact, Subscription, Payment
 from decimal import Decimal
 COUNTER = Decimal(0.01)
@@ -15,6 +18,13 @@ def nextAmount(current_amount):
 
     return current_amount
 
+def generateBase64UPIData(pa,pn,tn,am):
+    return base64.b64encode(bytes(json.dumps({
+        "pa": pa,
+        "pn": pn,
+        "tn": tn,
+        "am": am
+      }), "utf-8"))
 
 def get_settlement_status(group, cycle):
     payments = Payment.objects.filter(group=group, cycle=cycle)
@@ -27,3 +37,20 @@ def get_settlement_status(group, cycle):
         else:
             unpaid.append(sub)
     return paid, unpaid
+
+def createUPILink(upi_id, name, tx_note, amount):
+    headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache',
+    }
+
+    base64_data = generateBase64UPIData(upi_id, name, tx_note, amount)
+
+    data = {
+    '{"long_url":"https://upi.link/l?d': '{'+base64_data+'}"}'
+    }
+
+    response = requests.post('https://i9ag6sj2r4.execute-api.ap-south-1.amazonaws.com/default/generateShortLink', headers=headers, data=data)
